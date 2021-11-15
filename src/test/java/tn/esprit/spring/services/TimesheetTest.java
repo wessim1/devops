fc65cc67914b7528e2e442cee4b54ee2310fce37
ahmedabar <94314366+ahmedabar@users.noreply.github.com>
2021-11-15 03:04:20 +0100

    test2

--- src/test/java/tn/esprit/spring/services/TimesheetTest.java
+++ src/test/java/tn/esprit/spring/services/TimesheetTest.java
@@ -52,5 +52,63 @@ public class TimesheetTest {
 
         Assert.assertNotNull(sTimesheet.getMissionById(id));
     }
+    @Test
+    @Order(2)
+    public void testGetMissionById() {
 
+        Mission mission1 = new Mission("Inspection", "sur terrain");
+        int id = sTimesheet.ajouterMission(mission1);
+
+        Mission mission2 = sTimesheet.getMissionById(id);
+        Assert.assertNotNull(mission2);
+
+        Mission mission3 = sTimesheet.getMissionById(213232);
+        Assert.assertNull(mission3);
+    };
+
+    @Test
+    @Order(3)
+    public void testAffecterMissionADepartement() {
+        Mission mission = new Mission("Inspection", "sur terrain");
+        int idMission = sTimesheet.ajouterMission(mission);
+
+        Departement departement = new Departement("Info");
+        int idDepartement = sEntreprise.ajouterDepartement(departement);
+
+        int idDepAffecte = sTimesheet.affecterMissionADepartement(idMission, idDepartement);
+        Assert.assertEquals(idDepAffecte, idDepartement);
+
+    }
+/*
+    @Test
+    @Order(4)
+    public void testAjouterTimesheet() {
+        Mission mission = new Mission("Inspection", "sur terrain");
+
+        Employe employe = new Employe("Abar", "ahmed", "Ahmed.abar@esprit.tn", true, Role.INGENIEUR);
+        Date dateDebut = new Date(System.currentTimeMillis());
+        Date dateFin = new Date(System.currentTimeMillis());
+        sTimesheet.ajouterTimesheet(mission.getId(), employe.getId(), dateDebut, dateFin);
+    }
+*/
+    @Test
+    @Order(4)
+    public void testValiderTimesheet() {
+        Mission mission = new Mission("Inspection", "sur terrain");
+        sTimesheet.ajouterMission(mission);
+
+        Employe ingenieur = new Employe("Abar", "ahmed", "Ahmed.Abar@esprit.tn", true, Role.INGENIEUR);
+        Employe chef = new Employe("Abar", "ahmed", "Ahmed.Abar@esprit.tn", true, Role.CHEF_DEPARTEMENT);
+
+        Date dateDebut = new Date(System.currentTimeMillis());
+        Date dateFin = new Date(System.currentTimeMillis());
+
+        sEmploye.addOrUpdateEmploye(ingenieur);
+        sEmploye.addOrUpdateEmploye(chef);
+
+        assertEquals(0,sTimesheet.validerTimesheet(mission.getId(), ingenieur.getId(), dateDebut, dateFin, chef.getId()));
+        assertEquals(-1,
+                sTimesheet.validerTimesheet(mission.getId(), ingenieur.getId(), dateDebut, dateFin, ingenieur.getId()));
+
+    }
 }
